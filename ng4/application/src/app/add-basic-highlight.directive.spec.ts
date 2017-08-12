5db6c9fda207925bed7620cf0e327f0152f4b9dc
habibalsaki <habibalsaki@gmail.com>
2017-08-12 12:11:02 +0600

    adding ng4

---
+++ ng4/application/src/app/add-basic-highlight.directive.spec.ts
@@ -0,0 +1,8 @@
+import { AddBasicHighlightDirective } from './add-basic-highlight.directive';
+
+describe('AddBasicHighlightDirective', () => {
+  it('should create an instance', () => {
+    const directive = new AddBasicHighlightDirective();
+    expect(directive).toBeTruthy();
+  });
+});
